c42aa03fb7d37968c6f464fa28c2a2fb5ff693ac
Nicholas Carty <nickcarty13@gmail.com>
2018-12-17 16:02:33 -0500

    Added Card component and some styles

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
   constructor(props) {
@@ -30,7 +31,7 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album} />
     );
   }
 
